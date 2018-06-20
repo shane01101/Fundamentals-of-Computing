d1fdda962de3b193d9430a5a074eea119cbe84cd
Shane Honanie <shaneh425@gmail.com>
2018-06-20 02:50:08 -0700

    2048 Merge Updated

--- Principles of Computing/2048Merge.py
+++ Principles of Computing/2048Merge.py
@@ -1,4 +1,6 @@
 """
+Shane Honanie
+http://www.codeskulptor.org/#user44_ei6zN67ykK_1.py
 Merge function for 2048 game.
 """
 
@@ -7,21 +9,21 @@ def merge(line):
     Function that merges a single row or column in 2048.
     """
     result = []
+    last_added = False
     index_next_available = 0
-    for i in range(len(line)):
-        if line[i] > 0:
-            result.insert(index_next_available,line[i])
+    for idx in range(len(line)):
+        if line[idx] > 0:
+            result.insert(index_next_available,line[idx])
             
-            if i > 0 and result[index_next_available] == result[index_next_available - 1]:
+            if idx > 0 and  not last_added and result[index_next_available] == result[index_next_available - 1]:
                 result[index_next_available - 1] *= 2
+                last_added = True
                 result.pop(index_next_available)
                 result.insert(len(result), 0)
-                
-            index_next_available += 1
+            else:
+                index_next_available += 1
+                last_added = False
         else:
-            result.insert(len(result), line[i])
-        
+            result.insert(len(result), line[idx])
     
     return result
-
-print merge([2,0,2,4])
